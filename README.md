5f4631d2b92a77a736e66d386168d2ef4ae6ed73
Oluwakorede Fashokun <koredefashokun@gmail.com>
2020-07-28 20:46:19 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Pecunia
+
+Pecunia is a crypto wallet that focuses on enabling developers to create beautiful mobile-first dApps.
---
+++ app/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const App = () => {
+  return (
+    <View style={{ flex: 1 }}>
+      <Text></Text>
+    </View>
+  );
+};
+
+export default App;
