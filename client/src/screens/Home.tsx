ab504531fa812df61dbecbce0ae700f58ba69920
Oluwakorede Fashokun <koredefashokun@gmail.com>
2021-11-16 01:58:45 -0700

    Remove wallet address from home

--- client/src/screens/Home.tsx
+++ client/src/screens/Home.tsx
@@ -1,19 +1,15 @@
 import React from 'react';
-import { View, Text, StyleSheet, Pressable } from 'react-native';
+import { View, StyleSheet, Pressable } from 'react-native';
 import { useNavigation } from '@react-navigation/native';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { AppStackParamList } from '../types/navigation';
-import { useAppSelector } from '../redux/store';
 import Icon from '../components/Icon';
 
 const Home = () => {
   const { navigate } = useNavigation<StackNavigationProp<AppStackParamList>>();
-  const walletAddress = useAppSelector(({ wallet }) => wallet.address);
 
   return (
     <View style={styles.container}>
-      {walletAddress && <Text>Wallet address: {walletAddress}</Text>}
-
       <Pressable onPress={() => navigate('New')}>
         <Icon name='plus' />
       </Pressable>
