bff75d137b938ca6f88d7450668676bb61ced6b2
Oluwakorede Fashokun <koredefashokun@gmail.com>
2021-11-19 04:46:59 -0700

    Small style changes

--- client/src/components/contacts/ContactRow.tsx
+++ client/src/components/contacts/ContactRow.tsx
@@ -1,23 +1,50 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
 import { Contact } from '../../redux/contacts/types';
+import { truncateAddress } from '../../utils/address';
 
 interface ContactRowProps {
   contact: Contact;
 }
 
-const ContactRow: React.FC<ContactRowProps> = ({ contact }) => {
-  return (
-    <View>
-      <View>
-        <Text>{contact.name[0]}</Text>
-      </View>
-      <View>
-        <Text>{contact.name}</Text>
-        <Text>{contact.address}</Text>
-      </View>
+const ContactRow: React.FC<ContactRowProps> = ({ contact }) => (
+  <View style={styles.container}>
+    <View style={styles.avatar}>
+      <Text style={styles.avatarText}>{contact.name[0]}</Text>
     </View>
-  );
-};
+    <View>
+      <Text style={styles.name}>{contact.name}</Text>
+      <Text style={styles.address}>{truncateAddress(contact.address)}</Text>
+    </View>
+  </View>
+);
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row'
+  },
+  avatar: {
+    height: 50,
+    width: 50,
+    borderRadius: 25,
+    marginRight: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#505050'
+  },
+  avatarText: {
+    fontSize: 18,
+    color: '#FFFFFF',
+    fontWeight: '500'
+  },
+  name: {
+    fontSize: 16,
+    fontWeight: '500'
+  },
+  address: {
+    fontSize: 16,
+    color: '#D3D3D3'
+  }
+});
 
 export default ContactRow;
---
+++ client/src/utils/address.ts
@@ -0,0 +1,3 @@
+export const truncateAddress = (address: string) => {
+  return address;
+};
