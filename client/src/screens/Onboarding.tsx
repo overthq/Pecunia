5afeb7efb884b603701141bacb56472dd7720fae
Oluwakorede Fashokun <koredefashokun@gmail.com>
2021-11-20 02:39:49 -0700

    Add navigation button to onboarding

--- client/src/screens/Onboarding.tsx
+++ client/src/screens/Onboarding.tsx
@@ -1,13 +1,35 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { StyleSheet, Text } from 'react-native';
+import { NavigationProp, useNavigation } from '@react-navigation/core';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import Button from '../components/Button';
+import { AppStackParamList } from '../types/navigation';
 
 const Onboarding = () => {
+  const { navigate } = useNavigation<NavigationProp<AppStackParamList>>();
+
+  const createWallet = () => {
+    console.log('foo');
+  };
+
+  const goToImport = () => {
+    navigate('Import');
+  };
+
   return (
-    <SafeAreaView style={{ flex: 1 }}>
+    <SafeAreaView style={styles.container}>
       <Text>Onboarding</Text>
+      <Button text='Create wallet' onPress={createWallet} />
+      <Button text='Import wallet' onPress={goToImport} />
     </SafeAreaView>
   );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingHorizontal: 8
+  }
+});
 
 export default Onboarding;
